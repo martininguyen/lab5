ad9010331b7d287f863cda8608ada89ea3f61b20
Martin Nguyen <martinnguyen0618@gmail.com>
2016-02-04 13:57:58 -0800

    first push

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,5 @@ var data = require('../data.json');
 
 exports.view = function(req, res){
 	console.log(data);
-	res.render('index',{
-		'friends':[
-		{
-			'image': 'http://lorempixel.com/500/500/people',
-			'name' : 'Doug Engelbart',
-			'desc' : 'All my friends are imaginary.'
-		},
-		{
-			'image': 'http://lorempixel.com/500/500/people',
-			'name' : 'Ivan Sutherland',
-			'desc' : 'All my friends are imaginary.'
-		},
-		{
-			'image': 'http://lorempixel.com/500/500/people',
-			'name' : 'JCR Licklider',
-			'desc' : 'All my friends are imaginary.'
-		},
-		]
-	});
+	res.render('index', data);
 };
