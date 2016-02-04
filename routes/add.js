1c626de2cb2c1ccfb34c72b8e68597cdf1eadcd4
Martin Nguyen <martinnguyen0618@gmail.com>
2016-02-04 13:52:38 -0800

    first push

--- routes/add.js
+++ routes/add.js
@@ -2,10 +2,10 @@ var data = require("../data.json");
 
 exports.addFriend = function(req, res) {  
 	// Your code goes here
-	var newFriend ={
+	var newFriend = {
 		"name": req.query.name,
 		"description:" : req.query.description,
-		"imageURL" : "http://lorempixel.com/400/400/people",
+		"image" : "http://lorempixel.com/400/400/people",
 	}
 	
 	data["friends"].push(newFriend);
